3ac3794013ae0d4ab9a69f51fde4f6d4b95ec680
hayit08 <72305423+hayit08@users.noreply.github.com>
2021-01-29 00:23:19 +0500

    назар

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
-import Content from './components/Content/Content.jsx';
 import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
+import {  Route  } from 'react-router';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Content />
-
+      <div class='app-wrapper-content'>
+        
+      <Route component="Dialogs"/>
+      </div>
     </div>
   );
 }
