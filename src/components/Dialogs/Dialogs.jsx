2d9db59b6770724943af0f2d1410254d7f8d2a40
hayit08 <72305423+hayit08@users.noreply.github.com>
2021-01-29 11:27:33 +0500

    change style of dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 
-const Dialogs =(props)=> {
+const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Hayko
+            </div>
+                <div className={s.dialog}>
+                    Nazar
+            </div>
+                <div className={s.dialog}>
+                    Aman
+            </div>
+                <div className={s.dialog}>
+                    Meret
+            </div>
+                <div className={s.dialog}>
+                    Valera
+            </div>
+                <div className={s.dialog}>
+                    Dimych
+            </div>
+            </div>
+                <div className={s.messages}>
+                    <div className={s.dialog}>Hello</div>
+                    <div className={s.dialog}>How are you?</div>
+                    <div className={s.dialog}>Whatsapp</div>
+                
+            </div>
         </div>
     )
 }
