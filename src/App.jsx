28f48b279cfccd6c92c0a3039863b4c1f832e6c0
hayit08 <72305423+hayit08@users.noreply.github.com>
2021-01-29 10:24:37 +0500

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import Content from './components/Content/Content';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
-import {  Route  } from 'react-router';
 
-const App = () => {
+const App = (props) => {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
       <Header />
       <Navbar />
       <div class='app-wrapper-content'>
-        
-      <Route component="Dialogs"/>
+       
+      <Route path='/dialogs' component={Dialogs}/>
+      <Route path='/content' component={Content}/>
+      
+      
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
